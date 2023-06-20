f8b8bf8530748f859f67d48d0f1c50c5cead2c96
Coding SA <codingwebsa@gmail.com>
2023-06-20 23:23:41 +0600

    customized scrollbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,12 @@ import TopPublications from "~/components/top-publications";
 export default function Home() {
   return (
     <div>
-      <TopCategories />
-      <TopPublications />
+      <section>
+        <TopCategories />
+      </section>
+      <section>
+        <TopPublications />
+      </section>
     </div>
   );
 }
