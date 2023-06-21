2bb6f8a75e9db6bcb50eb56a55458864af832406
Coding SA <codingwebsa@gmail.com>
2023-06-21 14:37:32 +0600

    created top-searches

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@ import TopCategories from "~/components/top-categories";
 import TopPublications from "~/components/top-publications";
 import BookSection from "~/components/book/section";
 import DiscountSection from "~/components/discount-section";
+import SuggestedSearches from "~/components/suggested-searches";
 
 export default function Home() {
   return (
     <div>
+      <section className="mt-3">
+        <SuggestedSearches />
+      </section>
       <section>
         <TopCategories />
       </section>
@@ -18,9 +22,9 @@ export default function Home() {
       <section className="mt-6">
         <DiscountSection />
       </section>
-      {/* <section className="mt-10">
+      <section className="mt-10">
         <BookSection title="Weekly Popular Products" />
-      </section> */}
+      </section>
     </div>
   );
 }
---
+++ src/components/suggested-searches.tsx
@@ -0,0 +1,57 @@
+export default function SuggestedSearches() {
+  return (
+    <div>
+      <div className="container px-4">
+        <div className="flex snap-x snap-mandatory gap-3 overflow-auto pb-3">
+          {suggested_searches.map((search, i) => (
+            <span
+              className="flex-shrink-0 cursor-pointer snap-start rounded-full border border-stone-300 px-6 py-3 font-semibold duration-300 hover:border-green-100 hover:bg-green-100"
+              key={i}
+            >
+              <p>{search.label}</p>
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const suggested_searches = [
+  {
+    label: "কোরআন",
+  },
+  {
+    label: "শয়তানের নীলনকশা",
+  },
+  {
+    label: "আরিফ আজাদ",
+  },
+  {
+    label: "সমকালীন প্রকাশন",
+  },
+  {
+    label: "টাইম ম্যানেজমেন্ট",
+  },
+  {
+    label: "বেলা ফুরাবার আগে",
+  },
+  {
+    label: "ফেরা",
+  },
+  {
+    label: "এপিটাফ",
+  },
+  {
+    label: "নফসের বিরুদ্ধে লড়াই",
+  },
+  {
+    label: "ম্যাসেজ",
+  },
+  {
+    label: "আফিয়া সিদ্দিকী",
+  },
+  {
+    label: "সত্যায়ন প্রকাশন",
+  },
+];
